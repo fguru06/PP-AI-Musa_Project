4c2f872f4bea9c7b1d0688cfb42a3ae0babfa49e
FAREED HASSAN <fhassan06@gmail.com>
2026-03-06 08:06:15 -0500

    Update base path for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/Powerpoint-tool/',
+  base: '/PP-AI-Authoring-Tool/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
